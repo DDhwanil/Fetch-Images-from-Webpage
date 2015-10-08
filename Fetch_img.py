5316f1ab03622d85810b0ef44c9ed691ad6de1bf
root <root@localhost.localdomain>
2015-10-08 15:57:21 +0530

    Version 2

--- Fetch_img.py
+++ Fetch_img.py
@@ -10,14 +10,24 @@ ab.anonymize()
 url=raw_input('Enter url :http://')
 url='http://'+url
 page=ab.open(url).read()
+o=raw_input('Enter 1 for Windows os and 2 for Linux :')
+if o=='1':
+    dir=raw_input('Enter location to write images :')
+if o=='2':
+    print '\n\n Directory\n',
+    dir=str(subprocess.check_output(["pwd"])).rstrip('\n')
+    dir=dir+'/images'
+    print dir
+
 soup = BeautifulSoup(page)
 soup.find_all('img src')
 soup.find_all("title")
-dir=str(subprocess.check_output(["pwd"])).rstrip('\n')
-dir=dir+'/images'
+
+
 print soup.a
 print soup.title
 for link in soup.find_all('img'):
+
     filename=link['src'].lstrip('http://')
     filename=os.path.join(dir,filename.replace('/','_'))
     print filename
